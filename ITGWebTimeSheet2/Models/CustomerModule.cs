4da160cc37b0f649081e78449a18a8050c47e4c0
roqz2019 <59872961+roqz2019@users.noreply.github.com>
2020-05-06 22:30:13 +0800

    May_6_2020
    May_6_2020

---
+++ ITGWebTimeSheet2/Models/CustomerModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITGWebTimeSheet2.Models
+{
+    public class CustomersModule
+    {
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Customers name")]
+        public string name { get; set; }
+
+        public string id { get; set; }
+
+        public string code { get; set; }
+
+
+    }
+}
