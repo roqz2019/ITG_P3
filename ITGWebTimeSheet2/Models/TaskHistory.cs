b20c30a54552136e9aa1465e2813163e86106620
Alistair Bugay <alistair.bugay@iwestgroup.com>
2020-06-07 17:35:02 -0400

    created new model for task history

---
+++ ITGWebTimeSheet2/Models/TaskHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITGWebTimeSheet2.Models
+{
+    public class TaskHistory
+    {
+        public int id { get; set; }
+        public int planner_id { get; set; }
+
+        public string history { get; set; }
+        public DateTime datecreated { get; set; }
+    }
+}
