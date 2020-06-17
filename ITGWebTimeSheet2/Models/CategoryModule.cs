3a48f04391334ab27668a3dacf1330674743628f
Alistair Bugay <alistair.bugay@iwestgroup.com>
2020-06-17 16:02:11 -0400

    created new class module for category to hold category data

---
+++ ITGWebTimeSheet2/Models/CategoryModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITGWebTimeSheet2.Models
+{
+    public class CategoryModule
+    {
+        public string id { get; set; }
+        public string projectid { get; set; }
+        public string status { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public DateTime datecreated { get; set; }
+    }
+}
